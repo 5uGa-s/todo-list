f1a42772944f69c2429c5ce67f748118c9460881
菅沼さゆり <>
2023-08-19 15:07:31 +0900

    Edit, Delete 機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'accesses/goodbye'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'articles#index'
+  resources :articles, only: [:show]
 end
