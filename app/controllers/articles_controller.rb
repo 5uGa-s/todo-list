e600949885714b5b5b848f6b0280400ad0423a5c
菅沼さゆり <>
2023-08-19 19:13:53 +0900

    New Board ページの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
   def edit
     @article = Article.find(params[:id])
   end
