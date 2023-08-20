6c4a9cd6a4aa63f6bd4a8afdbdb008dd0e904881
菅沼さゆり <>
2023-08-20 20:51:59 +0900

    出力した内容表示、はみ出る文字のshowページのCSSの編集修正。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,21 @@ class ArticlesController < ApplicationController
     @article = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,4 +29,11 @@ class ArticlesController < ApplicationController
     article.destroy!
     redirect_to root_path
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+
 end
