6687db6b70096e06f1ec959306c735091843c803
菅沼さゆり <>
2023-08-20 21:39:14 +0900

    update機能の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if  @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     article = Article.find(params[:id])
     article.destroy!
