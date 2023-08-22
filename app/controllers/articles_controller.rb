260ccae91ebc7167d8cc8b2c844ec63cb13960c7
菅沼さゆり <>
2023-08-22 22:27:54 +0900

    noticeの修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,9 +16,9 @@ class ArticlesController < ApplicationController
   def create
     @article = current_user.articles.build(article_params)
     if @article.save
-      redirect_to article_path(@article), notice: '保存出来ました'
+      redirect_to article_path(@article), notice: '保存出来ました。'
     else
-      flash.now[:error] = '保存出来ませんでした'
+      flash.now[:error] = '保存出来ませんでした。'
       render :new
     end
   end
