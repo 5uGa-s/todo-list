a5eac7cb0c7558ae5f9f71c503481a118c3cedc2
菅沼さゆり <>
2023-08-23 18:00:51 +0900

    エラーメッセージの修正、エラーメッセージがフェイドアウトする実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,28 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$(document).ready(function() {
+  setTimeout(function() {
+    $('.notice').fadeOut('slow');
+  }, 2000);
+});
+
+$(document).ready(function() {
+  setTimeout(function() {
+    $('.error-message').fadeOut('slow');
+  }, 3000);
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
