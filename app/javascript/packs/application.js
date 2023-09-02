0df608f0c2a4f5792c212367e81f330d517413db
菅沼さゆり <>
2023-09-02 22:23:37 +0900

    ログアウト機能の修正、コメント欄の改行を適用出来るように修正、ログアウトした時はsign up, sign inを表示するよう修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,21 +15,10 @@ require("channels")
 
 $(document).ready(function() {
   setTimeout(function() {
-    $('.notice').fadeOut('slow');
-  }, 1000);
-});
-
-$(document).ready(function() {
-  setTimeout(function() {
-    $('.error-message').fadeOut('slow');
+    $('.notice, .error-message, .alert').fadeOut('slow');
   }, 2000);
 });
 
-$(document).ready(function() {
-  setTimeout(function() {
-    $('.alert').fadeOut('slow');
-  }, 2000);
-});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'accesses/hello'
-  get 'accesses/goodbye'
+  #get 'accesses/hello'
+  #get 'accesses/goodbye'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
