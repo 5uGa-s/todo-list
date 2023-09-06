f21095a6c0d0305023d36e5e4f0a9d8afe2f922a
菅沼さゆり <>
2023-09-06 19:09:56 +0900

    画像が追加出来るように修正。showページのデザインの修正。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
 
   def set_article
-    @article = current_user.articles.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def index
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = '⚪︎保存出来ました。'
       redirect_to article_path(@article)
     else
-      flash.now[:error] = '⚪ﾀｲﾄﾙは1字以上20字以内、内容は3字以上入力して下さい。'
+      flash.now[:error] = '⚪ﾀｲﾄﾙは1字以上25字以内、内容は3字以上入力して下さい。'
       render :new
     end
   end
@@ -50,13 +50,11 @@ class ArticlesController < ApplicationController
     redirect_to root_path
   end
 
+
+
   private
   def article_params
-    params.require(:article).permit(:title, :content)
-  end
-
-  def article_params
-    params.require(:article).permit(:title, :image)
+    params.require(:article).permit(:title, :content, :image,)
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,11 +13,22 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+//エラーメッセージ
+
 $(document).ready(function() {
   setTimeout(function() {
     $('.notice, .error-message, .alert').fadeOut('slow');
-  }, 2000);
+  }, 1000);
+
+//タイトルの全体の表示
+$(document).ready(function() {
+  $('.boards-title-show').click(function() {
+    $(this).next('.boards-title-full').toggle(); // クリックで表示切り替え
+  });
 });
+
+});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
